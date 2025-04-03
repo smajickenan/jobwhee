746a3b20d72786a7e2fd5cf525856b780772f373
Kenan Smajic <ksmajic996@gmail.com>
2025-04-03 21:52:48 +0200

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jobwhee - Find Talent & Get The Job Done",
+  description: "Find talent, sign up, and get the job done with Jobwhee",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
